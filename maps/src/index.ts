37255c547cbbcb983995446b0786a40edd1035ca
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2022-01-05 00:52:47 -0800

    generate map on page

---
+++ maps/src/index.ts
@@ -0,0 +1,10 @@
+// import { User } from './User';
+// import { Company } from './Company';
+import { Map } from './Map'
+
+// const user = new User();
+// const company = new Company();
+
+new Map('map');
+
+
