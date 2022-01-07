f7ff9e7e8823b910ff29d12b4f2798f0379e4c8c
Ken Nichol <1321022+kennithnichol@users.noreply.github.com>
2022-01-07 00:18:40 -0800

    create an HTML reporter

--- stats/src/index.ts
+++ stats/src/index.ts
@@ -4,10 +4,18 @@ import { WinsAnalysis } from "./analyzers/WinsAnalysis";
 import { ConsoleReport } from "./reportTargets/ConsoleReport";
 import { Summary } from "./Summary";
 import { Console } from "console";
+import { HtmlReport } from "./reportTargets/HtmlReport";
 
 const csvFileReader = new CsvFileReader('football.csv');
 const matchReader = new MatchReader(csvFileReader);
 matchReader.load();
 
-const manUWins = new Summary(new WinsAnalysis('Man United'), new ConsoleReport());
-manUWins.buildAndPrintReport(matchReader.matches)
+const manUnitedWinsHtml = new Summary(
+	new WinsAnalysis('Man United'),
+	new HtmlReport());
+manUnitedWinsHtml.buildAndPrintReport(matchReader.matches);
+
+const manUWins = new Summary(
+	new WinsAnalysis('Man United'),
+	new ConsoleReport());
+manUWins.buildAndPrintReport(matchReader.matches);
---
+++ stats/src/reportTargets/HtmlReport.ts
@@ -0,0 +1,21 @@
+import fs from 'fs';
+import { OutputTarget } from "../Summary";
+
+export class HtmlReport implements OutputTarget {
+	print(report: string): void {
+		const html = `<html>
+		<head>
+			<title>Analysis Output</title>
+		</head>
+		<body>
+			<div>
+				<h1>Analysis Output</h1>
+				<div>${report}</div>
+			</div>
+		</body>
+		</html>
+		`
+
+		fs.writeFileSync('report.html', html)
+	}
+}
